03342c4678019118f8434f430efd8e6cf92ccb02
Douglas Orr <douglaso@graphcore.ai>
2023-05-23 11:19:52 +0000

    Quantisation overhaul to support grouped formats, multi-level schemes, etc

--- llminference/quantisation.py
+++ llminference/quantisation.py
@@ -1,15 +1,30 @@
 """Utilities for "fake quantisation"."""
 
+import math
 import re
 from dataclasses import dataclass
-from typing import Optional, Union, cast
+from typing import Optional, Sequence, Tuple, cast
 
 import torch
 from torch import Tensor, nn
 
+Shape = Tuple[int, ...]
+
+
+class TensorFormat:
+    """Quantisation formats for tensors."""
+
+    def quantise(self, tensor: Tensor) -> Tensor:
+        raise NotImplementedError
+
+    def count_bits(self, shape: Shape) -> int:
+        raise NotImplementedError
+
 
 @dataclass
-class Format:
+class Format(TensorFormat):
+    """Elementwise scalar formats."""
+
     exponent_bits: int
     mantissa_bits: int
 
@@ -17,28 +32,15 @@ class Format:
     def max_absolute_value(self) -> float:
         raise NotImplementedError
 
-    def quantise(self, x: Tensor) -> Tensor:
-        raise NotImplementedError
-
     @property
     def bits(self) -> int:
         return 1 + self.exponent_bits + self.mantissa_bits
 
+    def count_bits(self, shape: Shape) -> int:
+        return self.bits * math.prod(shape)
+
     def __str__(self) -> str:
         return f"E{self.exponent_bits}M{self.mantissa_bits}"
-
-    @staticmethod
-    def parse(value: str) -> Union["FPFormat", "IntFormat"]:
-        m = re.match(r"^E(\d+)M(\d+)$", value)
-        if not m:
-            raise ValueError(f"Couldn't parse {value!r}")
-        exponent_bits = int(m.group(1))
-        mantissa_bits = int(m.group(2))
-        if exponent_bits == 0:
-            return IntFormat(0, mantissa_bits)
-        if exponent_bits >= 2:
-            return FPFormat(exponent_bits, mantissa_bits)
-        raise ValueError(f"No format {value!r} available (note: E1M6 == E0M7)")
 
 
 @dataclass
@@ -82,6 +84,27 @@ class FPFormat(Format):
 
 
 @dataclass
+class IEEEFormat(Format):
+    def __post_init__(self) -> None:
+        assert (self.exponent_bits, self.mantissa_bits) in [(5, 10), (8, 23)]
+
+    @property
+    def max_absolute_value(self) -> float:
+        max_exponent = 2 ** (self.exponent_bits - 1) - 1
+        return cast(float, (2 - 2**-self.mantissa_bits) * 2**max_exponent)
+
+    def quantise(self, x: Tensor) -> Tensor:
+        assert x.dtype == torch.float32
+        if self.exponent_bits == 8:
+            return x
+        return (
+            torch.clip(x, -self.max_absolute_value, self.max_absolute_value)
+            .half()
+            .float()
+        )
+
+
+@dataclass
 class IntFormat(Format):
     def __post_init__(self) -> None:
         assert self.exponent_bits == 0, "IntFormat has no exponent bits"
@@ -96,79 +119,177 @@ class IntFormat(Format):
         )
 
 
-def quantisation_scale(
-    value: Tensor, format: Format, mode: str, global_bias: Optional[int] = None
-) -> Tensor:
-    """Get a scaling tensor to apply to quantise a given tensor.
+@dataclass
+class ExpCeilFormat(Format):
+    def __post_init__(self) -> None:
+        assert self.mantissa_bits == 0, "ExpCeilFormat has no exponent bits"
 
-    value -- shape (output_channels, input_channels)
+    @property
+    def bits(self) -> int:
+        return self.exponent_bits  # override to remove sign bit
 
-    mode -- "global|tensor|input|output|inout"
+    @property
+    def exponent_bias(self) -> float:
+        return 2.0 ** (self.exponent_bits - 1) - 1
 
-    global_bias -- only used when mode="global"
+    @property
+    def max_absolute_value(self) -> float:
+        return cast(float, 2 ** (2**self.exponent_bits - 1 - self.exponent_bias))
+
+    def quantise(self, x: Tensor) -> Tensor:
+        assert torch.all(x >= 0), f"{type(self)} does not support negative numbers"
+        y: Tensor = 2 ** torch.clip(
+            torch.ceil(torch.log2(x)),
+            -self.exponent_bias,
+            2**self.exponent_bits - 1 - self.exponent_bias,
+        )
+        return y
+
+
+FP32 = IEEEFormat(8, 23)
+FP16 = IEEEFormat(5, 10)
+
+
+def parse(value: str) -> Format:
+    if value == "FP16":
+        return FP16
+    if value == "FP32":
+        return FP32
+    m = re.match(r"^E(\d+)M(\d+)$", value)
+    if not m:
+        raise ValueError(f"Couldn't parse {value!r}")
+    exponent_bits = int(m.group(1))
+    mantissa_bits = int(m.group(2))
+    if exponent_bits == 0:
+        return IntFormat(0, mantissa_bits)
+    if exponent_bits >= 2:
+        return FPFormat(exponent_bits, mantissa_bits)
+    raise ValueError(f"No format {value!r} available (note: E1M6 == E0M7)")
+
+
+@dataclass
+class LinearScalingFormat(TensorFormat):
+    """Specifies a scheme for quantising tensors.
+
+    group_shapes -- list of groupings (size of groups in each dimension)
+                    e.g. [(1, 8)]                input-groups of size 8
+                         [(2, 2)]                square groups of 2x2 (4 elements)
+                         [(1, None)]             per-output-channel scaling
+                         [(None, 1), (None, 1)]  sqrt(input*output)-channel scaling
     """
-    if mode == "global":
-        assert global_bias
-        return torch.tensor(2**global_bias)
-    absvalue = value.abs()
-    if mode == "tensor":
-        return absvalue.max() / format.max_absolute_value
-    if mode == "input":
-        return cast(
-            Tensor, absvalue.max(dim=0, keepdim=True).values / format.max_absolute_value
-        )
-    if mode == "output":
-        return cast(
-            Tensor, absvalue.max(dim=1, keepdim=True).values / format.max_absolute_value
-        )
-    if mode == "inout":
-        return (
-            torch.sqrt(
-                absvalue.max(dim=0, keepdim=True).values
-                * absvalue.max(dim=1, keepdim=True).values
+
+    GroupShape = Tuple[Optional[int], ...]
+
+    element_format: Format
+    group_shapes: Sequence[GroupShape]
+    scale_format: TensorFormat
+
+    @staticmethod
+    def _group_shape_for(tensor_shape: Shape, group_shape: GroupShape) -> Shape:
+        assert len(tensor_shape) == len(group_shape), f"{tensor_shape} vs {group_shape}"
+        return tuple((t if g is None else g) for t, g in zip(tensor_shape, group_shape))
+
+    def count_bits(self, shape: Shape) -> int:
+        """Count the number of bits in the quantised representation."""
+        count = self.element_format.count_bits(shape)
+        for group_shape in self.group_shapes:
+            count += self.scale_format.count_bits(
+                tuple(
+                    t // g
+                    for t, g in zip(shape, self._group_shape_for(shape, group_shape))
+                )
             )
-            / format.max_absolute_value
+        return count
+
+    @staticmethod
+    def _group_scale_for(absratio: Tensor, group_shape: Shape) -> Tensor:
+        full_grouped_shape = tuple(
+            s
+            for size, group_size in zip(absratio.shape, group_shape)
+            for s in [size // group_size, group_size]
         )
-    raise ValueError(f"Unexpected quantisation_scale mode={mode}")
+        grouped_absratio = absratio.reshape(full_grouped_shape)
+        for dim in range(1, len(full_grouped_shape), 2):
+            grouped_absratio = grouped_absratio.max(dim=dim, keepdim=True).values
+        return grouped_absratio.broadcast_to(full_grouped_shape).reshape(absratio.shape)
+
+    def scale_for(self, tensor: Tensor) -> Tensor:
+        """Get the quantised scaling tensor to apply to quantise a given tensor."""
+        absratio = tensor.abs() / self.element_format.max_absolute_value
+        scale = torch.ones_like(absratio)
+        for group_shape in self.group_shapes:
+            scale *= self.scale_format.quantise(
+                self._group_scale_for(
+                    absratio, self._group_shape_for(absratio.shape, group_shape)
+                )
+            )
+        return torch.pow(scale, 1 / len(self.group_shapes))
+
+    def quantise(self, tensor: Tensor) -> Tensor:
+        """Quantise a tensor under the scheme."""
+        scale = self.scale_for(tensor)
+        return self.element_format.quantise(tensor / scale) * scale
 
 
-def quantisation_bits(
-    value: Tensor, format: Format, mode: str, scaling_format_bits: int = 16
-) -> int:
-    """Count the number of bits in a quantised representation of a tensor."""
-    count = format.bits * value.nelement()
-    if mode == "tensor":
-        count += scaling_format_bits
-    if mode in ["input", "inout"]:
-        count += scaling_format_bits * value.shape[1]
-    if mode in ["output", "inout"]:
-        count += scaling_format_bits * value.shape[0]
-    return count
+def tensor_scaling_format(
+    element_format: Format, scale_format: Format = FP16
+) -> LinearScalingFormat:
+    """A per-(2D)-tensor scaling format."""
+    return LinearScalingFormat(element_format, [(None, None)], scale_format)
+
+
+def channel_scaling_format(
+    element_format: Format, per: str, scale_format: TensorFormat = FP16
+) -> LinearScalingFormat:
+    """A per-channel scaling format.
+
+    per -- "input|output|inout"
+    """
+    groups = cast(
+        Sequence[LinearScalingFormat.GroupShape],
+        dict(input=[(None, 1)], output=[(1, None)], inout=[(None, 1), (1, None)])[per],
+    )
+    return LinearScalingFormat(element_format, groups, scale_format)
+
+
+def group_scaling_format(
+    element_format: Format,
+    grouping: str,
+    group_size: int,
+    scale_format: TensorFormat = FP16,
+) -> LinearScalingFormat:
+    """A simplified grouped scaling format: only 1D groups & no "inout" product.
+
+    groups -- "input|output"
+    """
+    return LinearScalingFormat(
+        element_format,
+        dict(input=[(1, group_size)], output=[(group_size, 1)])[grouping],
+        scale_format,
+    )
 
 
 def quantise_model(
     model: nn.Module,
-    format: Format,
-    mode: str,
-    global_bias: Optional[int] = None,
-    unquantised_format_bits: int = 16,
+    formats: Sequence[Tuple[str, TensorFormat]] = [],
+    vector_format: TensorFormat = FP16,
 ) -> float:
-    """In-place quantise a model, returning the size of the quantised model (bytes)."""
+    """In-place quantise a model, returning the size of the quantised model (bytes).
+
+    formats -- [(str -- PATTERN, TensorFormat)] -- for each parameter, use the first
+               format for which NAME matches PATTERN
+    """
     bitcount = 0
     for name, p in model.named_parameters():
         if p.ndim == 1:
-            # Don't quantise biases/element-scales
-            bitcount += p.nelement() * unquantised_format_bits
-        elif p.ndim == 2:
-            scale = quantisation_scale(
-                p.data, format, mode=mode, global_bias=global_bias
-            )
-            p.data = format.quantise(p.data / scale) * scale
-            bitcount += quantisation_bits(
-                p.data, format, mode=mode, scaling_format_bits=unquantised_format_bits
-            )
+            format_ = vector_format
         else:
-            raise ValueError(
-                f"Don't know how to quantise a rank-{p.ndim} tensor ({name})"
-            )
+            for pattern, pattern_format in formats:
+                if re.search(pattern, name):
+                    format_ = pattern_format
+                    break
+            else:
+                raise ValueError(f"No format matched tensor {name!r}")
+        p.data = format_.quantise(p.data)
+        bitcount += format_.count_bits(p.data.shape)
     return bitcount / 8
--- llminference/tests/test_quantisation.py
+++ llminference/tests/test_quantisation.py
@@ -1,26 +1,25 @@
 from typing import cast
 
 import torch
+import torch.nn.functional as F
 
 from .. import quantisation
 
 
 def test_int_format() -> None:
-    fmt = quantisation.Format.parse("E0M7")
+    fmt = quantisation.parse("E0M7")
     assert fmt.bits == 8
     assert fmt.max_absolute_value == 127
     assert set(fmt.quantise(torch.linspace(-150, 150, steps=1000)).tolist()) == set(
         range(-127, 128)
     )
     assert set(
-        quantisation.Format.parse("E0M3")
-        .quantise(torch.linspace(-10, 10, steps=100))
-        .tolist()
+        quantisation.parse("E0M3").quantise(torch.linspace(-10, 10, steps=100)).tolist()
     ) == set(range(-7, 8))
 
 
 def test_fp_format() -> None:
-    fmt = cast(quantisation.FPFormat, quantisation.Format.parse("E2M1"))
+    fmt = cast(quantisation.FPFormat, quantisation.parse("E2M1"))
     assert fmt.bits == 4
     assert fmt.max_absolute_value == 3
     assert fmt.min_absolute_normal == 0.5
@@ -29,8 +28,56 @@ def test_fp_format() -> None:
         sx for x in [0, 0.25, 0.5, 0.75, 1, 1.5, 2, 3] for sx in [x, -x]
     }
     assert set(
-        quantisation.Format.parse("E3M0")
+        quantisation.parse("E3M0")
         .quantise(torch.linspace(-10, 10, steps=1000))
         .abs()
         .tolist()
     ) == {0, 0.125, 0.25, 0.5, 1, 2, 4, 8}
+
+
+def test_ieee_fp16() -> None:
+    assert quantisation.FP16.max_absolute_value == 65504
+    assert torch.equal(
+        quantisation.FP16.quantise(
+            torch.tensor([2**-25 * 0.99, 2**-25 * 1.01, -1, 1e5])
+        ),
+        torch.tensor([0, 2**-24, -1, 65504.0]),
+    )
+
+
+def test_linear_scaling_format() -> None:
+    torch.manual_seed(23875)
+    tensor = torch.randn((10, 20))
+    e3m4 = quantisation.parse("E3M4")
+
+    # 1. Per-tensor scaling
+    per_tensor = quantisation.tensor_scaling_format(e3m4)
+    assert per_tensor.count_bits(tensor.shape) == 8 * 200 + 16
+    per_tensor_mse = F.mse_loss(per_tensor.quantise(tensor), tensor)
+
+    # 2. Per-output channel scaling
+    per_output_channel = quantisation.channel_scaling_format(e3m4, per="output")
+    assert per_output_channel.count_bits(tensor.shape) == 8 * 200 + 16 * 10
+    per_output_channel_mse = F.mse_loss(per_output_channel.quantise(tensor), tensor)
+    assert per_output_channel_mse < per_tensor_mse  # could be ==, but unlikely
+
+    # 3. Input-group scaling
+    input_group = quantisation.group_scaling_format(
+        e3m4, grouping="input", group_size=5
+    )
+    assert input_group.count_bits(tensor.shape) == 8 * 200 + 16 * 10 * (20 // 5)
+    input_group_mse = F.mse_loss(input_group.quantise(tensor), tensor)
+    assert input_group_mse < per_output_channel_mse  # could be ==, but unlikely
+
+    # 4. Others
+    for format_ in [
+        quantisation.channel_scaling_format(e3m4, per="output"),
+        quantisation.channel_scaling_format(e3m4, per="inout"),
+        quantisation.channel_scaling_format(
+            e3m4, per="input", scale_format=quantisation.tensor_scaling_format(e3m4)
+        ),
+    ]:
+        quantised = format_.quantise(tensor)
+        assert quantised.shape == tensor.shape
+        assert torch.all(~torch.isnan(quantised))
+        assert format_.count_bits(tensor.shape) > 8 * tensor.nelement()
