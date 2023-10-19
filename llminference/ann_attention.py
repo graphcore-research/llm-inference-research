506c795500f6bd3f82ae2609d4f6787f14652ad6
Douglas Orr <douglaso@graphcore.ai>
2023-10-19 12:34:44 +0000

    Add a convenience constructor for ann_attention.Settings

--- llminference/ann_attention.py
+++ llminference/ann_attention.py
@@ -84,6 +84,23 @@ class Settings:
     local_k: int
     score: ScoreSettings
 
+    def __init__(
+        self, k: int, local_k: int, score: Union[ScoreSettings, str], **args: Any
+    ):
+        if isinstance(score, str):
+            ctor: Any = dict(low_rank=LowRank.Settings, sparse_q=SparseQ.Settings)[
+                score
+            ]
+            score_settings: ScoreSettings = ctor(**args)
+        else:
+            assert (
+                not args
+            ), "ann_attention.Setting only accepts **args when `score` is a string"
+            score_settings = score
+        self.k = k
+        self.local_k = local_k
+        self.score = score_settings
+
 
 class ANN(nn.Module):
     """Generic ANN with local windowing and masking."""
