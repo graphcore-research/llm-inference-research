Fix masking in attention-reallocation, improve tests, enable for Pythia

llminference/ann_attention.py
@@ -134,6 +134,41 @@ class ANN(nn.Module):
         # Set to an empty list to turn on ANN index logging
         self.log_indices: Optional[List[Tensor]] = None
 
+    def _attention(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+        logmask: Tensor,
+        kv_weight: Tensor,
+        mean_value: Tensor,
+    ) -> Tuple[Tensor, Tensor]:
+        """Dense attention, with left-over weight reallocation.
+
+        query -- (batch, n_heads, n_query, head_size)
+
+        key -- (batch, n_heads, n_kv, head_size)
+
+        value -- (batch, n_heads, n_kv, head_size)
+
+        logmask -- (batch, n_heads, n_query, n_kv)
+
+        kv_weight -- (batch, n_heads, n_query) | ()
+                  -- 1.0 for regular attention (no reallocation)
+
+        mean_value -- (batch, n_heads, n_query, head_size)
+        """
+        weights = torch.softmax(
+            (query @ key.transpose(-1, -2)).div_(query.shape[-1] ** 0.5).add_(logmask),
+            -1,
+            dtype=torch.float32,
+        ).to(value.dtype)
+        # Value-mixing with reallocation
+        weights *= kv_weight[..., None]
+        output = weights @ value
+        output += (1 - kv_weight[..., None]) * mean_value
+        return output, weights
+
     def forward(
         self, query: Tensor, key: Tensor, value: Tensor, logmask: Tensor
     ) -> Tuple[Tensor, Tensor]:
@@ -170,32 +205,27 @@ class ANN(nn.Module):
         if self.log_indices is not None:
             self.log_indices.append(indices)
 
-        # Slice key, value, logmask
-        kv_indices = indices.squeeze(-2).unsqueeze(-1)
-        key = gather(key, -2, kv_indices)
-        mean_value = value.mean(-2, keepdim=True)  # before gather
-        value = gather(value, -2, kv_indices)
-        logmask = gather(logmask, -1, indices)
-
         # Optional "mean_value" kv
-        mean_value_weight = 0.0
+        value_mask = logmask.squeeze(-2).unsqueeze(-1).exp()
+        mean_value = ((value * value_mask).sum(-2) / value_mask.sum(-2)).unsqueeze(-2)
+        kv_weight = torch.tensor(1.0)
         if self.settings.add_remainder:
             assert self.settings.add_remainder == "v2"
-            mean_value_weight = (
-                1 - gather(torch.softmax(score, -1), -1, indices).sum(-1, keepdim=True)
-            ).to(value.dtype)
+            kv_weight = (gather(torch.softmax(score, -1), -1, indices).sum(-1)).to(
+                value.dtype
+            )
 
-        # Attention, with left-over weight reallocation
-        weights = torch.softmax(
-            (query @ key.transpose(-1, -2)).div_(query.shape[-1] ** 0.5).add_(logmask),
-            -1,
-            dtype=torch.float32,
-        ).to(value.dtype)
-        output = (1 - mean_value_weight) * (
-            weights @ value
-        ) + mean_value_weight * mean_value
-
-        return output, weights
+        # Slice key, value, logmask for attention
+        kv_indices = indices.squeeze(-2).unsqueeze(-1)
+        output, weights = self._attention(
+            query,
+            gather(key, -2, kv_indices),
+            gather(value, -2, kv_indices),
+            gather(logmask, -1, indices),
+            kv_weight=kv_weight,
+            mean_value=mean_value,
+        )
+        return output, torch.zeros_like(logmask).scatter(-1, indices, weights)
 
 
 class GPTNeoXAttentionWithANN(GPTNeoXAttention):  # type:ignore[misc]
@@ -212,11 +242,11 @@ class GPTNeoXAttentionWithANN(GPTNeoXAttention):  # type:ignore[misc]
         head_mask: Optional[Tensor] = None,
     ) -> Tuple[Tensor, Tensor]:
         assert attention_mask is not None
+        assert head_mask is None
 
         # Only enable ANN during autoregressive generation
         if query.shape[-2] == 1:
-            assert False, "TODO, I've broken this"
-            query, key, value, attention_mask = self.ann(
+            return self.ann(  # type:ignore[no-any-return]
                 query,
                 key,
                 value,
@@ -238,7 +268,7 @@ class LlamaAttentionWithANN(llama_attention.LlamaAttention):
         self, query: Tensor, key: Tensor, value: Tensor, logmask: Tensor
     ) -> Tuple[Tensor, Tensor]:
         if query.shape[-2] == 1:
-            return self.ann(
+            return self.ann(  # type:ignore[no-any-return]
                 query, key, value, logmask.broadcast_to(key.unsqueeze(-3).shape[:-1])
             )
         return super()._attn(query, key, value, logmask)

llminference/tests/test_ann_attention.py
@@ -1,6 +1,8 @@
 from typing import cast
 
 import torch
+from transformers.models.gpt_neox.configuration_gpt_neox import GPTNeoXConfig
+from transformers.models.llama.configuration_llama import LlamaConfig
 
 from .. import ann_attention as ann
 from .. import eval_adapter
@@ -11,31 +13,73 @@ def test_ann_module() -> None:
     query = torch.randn((batch, n_heads, 1, head_size))
     key = torch.randn((batch, n_heads, n_key, head_size))
     value = torch.randn((batch, n_heads, n_key, head_size))
-    # Mask contains leading `fmin`, different across the batch
-    logmask = torch.finfo(torch.float32).min * (
-        torch.arange(n_key)[None, None, None, :]
-        < torch.tensor([2, 0, 5])[:, None, None, None]
-    ).broadcast_to(batch, n_heads, 1, n_key)
+
+    # Mask out some leading token positions (different across the batch)
+    mask = (torch.tensor([2, 0, 5])[:, None] <= torch.arange(n_key))[
+        :, None, None, :
+    ].broadcast_to(batch, n_heads, 1, n_key)
+    key.masked_fill_(~mask[:, :, 0, :, None], 1e9)
+    value.masked_fill_(~mask[:, :, 0, :, None], float("inf"))
+
     for score, add_remainder in [
         (ann.LowRank.Settings(8), False),
-        (ann.SparseQ.Settings(6), True),
+        (ann.SparseQ.Settings(6), "v2"),
     ]:
         module = ann.ANN(
             ann.Settings(
                 k=4,
                 local_k=1,
-                add_remainder=add_remainder,
+                add_remainder=cast(bool, add_remainder),
                 score=cast(ann.ScoreSettings, score),
             ),
             n_heads,
             head_size,
         )
-        q, k, v, m = module(query, key, value, logmask)
-        assert torch.equal(q, query)
-        assert k.shape == (batch, n_heads, 4 + add_remainder, head_size)
-        assert v.shape == (batch, n_heads, 4 + add_remainder, head_size)
-        assert m.shape == (batch, n_heads, 1, 4 + add_remainder)
-        assert torch.isin(key[:, :, -1, :].flatten(), k.flatten()).all(), "local_k"
+        output, weights = module(query, key, value, mask.float().log())
+        assert not torch.isinf(output).any()
+        assert output.shape == (batch, n_heads, 1, head_size)
+        assert weights.shape == (batch, n_heads, 1, n_key)
+        # At most 4 nonzeros per (batch, head), but may be fewer (due to the mask)
+        assert ((weights != 0).sum(-1) == mask.sum(-1).minimum(torch.tensor(4))).all()
+        assert (weights[..., -1] != 0).all(), "local_k"
+        if add_remainder:
+            assert (weights.sum(-1) <= 1.00001).all()
+        else:
+            torch.testing.assert_close(weights.sum(-1), torch.ones((batch, n_heads, 1)))
+
+
+def test_gptneox_ann() -> None:
+    module = ann.GPTNeoXAttentionWithANN(
+        GPTNeoXConfig(hidden_size=128, num_attention_heads=4),
+        ann.Settings(k=8, local_k=2, add_remainder="v2", score="sparse_q", rank=12),
+    )
+    output, _, weights = module(
+        torch.randn(13, 1, 128),
+        attention_mask=torch.zeros(13, 1, 1, 20),
+        position_ids=torch.tensor([19])[None],
+        layer_past=(torch.randn(13, 4, 19, 32), torch.randn(13, 4, 19, 32)),
+        output_attentions=True,
+    )
+    assert output.shape == (13, 1, 128)
+    assert ((-1e3 <= output) & (output <= 1e3)).all()  # "reasonable" outputs
+    assert ((weights != 0).sum(-1) == 8).all()
+
+
+def test_llama_ann() -> None:
+    module = ann.LlamaAttentionWithANN(
+        LlamaConfig(hidden_size=128, num_attention_heads=4, num_key_value_heads=4),
+        ann.Settings(k=8, local_k=2, add_remainder="v2", score="sparse_q", rank=12),
+    )
+    output, weights, _ = module(
+        torch.randn(13, 1, 128),
+        attention_mask=torch.zeros(13, 1, 1, 20),
+        position_ids=torch.tensor([19])[None],
+        past_key_value=(torch.randn(13, 4, 19, 32), torch.randn(13, 4, 19, 32)),
+        output_attentions=True,
+    )
+    assert output.shape == (13, 1, 128)
+    assert ((-1e3 <= output) & (output <= 1e3)).all()  # "reasonable" outputs
+    assert ((weights != 0).sum(-1) == 8).all()
 
 
 def test_convert_gptneox() -> None: